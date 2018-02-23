aea305213b72b2d9d024a2f56d8efb85ce93cbca
Timothy Baker <tbake0155@gmail.com>
2018-02-23 01:08:30 -0500

    updates

--- popup.js
+++ popup.js
@@ -14,36 +14,38 @@ var fileName = currentURL.substring(currentURL.lastIndexOf('/'));
 fileName = fileName.replace("/",""); // we only the attributes for this file
 
 var attributes = "";
-var client = new XMLHttpRequest();
-client.open('GET', attributes_file);
-client.onreadystatechange = function() {
-   attributes = client.responseText; 
+var request = new XMLHttpRequest();
+request.open('GET', attributes_file, false);  // `false` makes the request synchronous
+request.send(null);
+
+if (request.status === 200) {
+  attributes = request.responseText;
+  
+}
+
 var lines = attributes.split('\n');
-for(var i = 0;i < lines.length;i++){
-    //code here using lines[i] which will give you each line
+
+console.log(lines);
+
+for(var i = 0;i < lines.length;i++)
+{
     if(lines[i] == "#ENTRY#")
     {
-        if(lines[i+1] == fileName)
-        {
-            var key = lines[i+2];
-            var val = lines[i+3];
-            i++;i++;i++;
+       if(lines[i+1] == fileName)
+       {
+           var key = lines[i+2];
+           var val = lines[i+3];
 
-            var re = new RegExp(key, "gi");
-            var injected = "<a href=\"https://www.google.com\">" + re + "</a>";
+           var re = new RegExp(key, "gi");
+           var injected = "<a href=\"https://www.google.com\">" + key + "</a>";
 
-            var mod_body = body.replace(re, injected);
+           var mod_body = body.replace(re, injected);
+           console.log(re);
+           console.log(injected);
         }
     }
 }
-    
+   
 doc = doc.replace(body, mod_body);
 document.body.innerHTML = doc;
-}
-client.send();
-
-
-
-
-
 
